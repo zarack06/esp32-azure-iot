2e2f7f956f6d9578c376b4627faa4e7604e275fe
kiemrss2@kiemrss2gmail.com <nguyen@nguyen003>
2025-12-28 17:02:05 +0700

    .

---
+++ src/azure_iot/azure_iot.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef AZURE_IOT_H
+#define AZURE_IOT_H
+#include <stdbool.h>
+#ifdef __cplusplus
+extern "C" {
+#endif  
+
+void azure_iot_init(void);
+void azure_iot_start(void);
+void azure_iot_send_telemetry(float temp, float hum);
+bool azure_iot_is_connected(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* AZURE_IOT_H */
